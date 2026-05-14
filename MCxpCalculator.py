ba58c408a0db3e8485ccf285a92ee950f67533d5
Fastburnerr <153125912+Fastburnerr@users.noreply.github.com>
2026-05-14 00:33:27 -0500

    Add files via upload

--- MCxpCalculator.py
+++ MCxpCalculator.py
@@ -4,7 +4,7 @@
 
  Description: A CLI tool to calculate the time to obtain an XP level given a mob,
               average rate of killing the mob, starting level, and the goal level.
-              The time is given in a format of days, hours, minutes, seconds".
+              The time is given in a format of days, hours, minutes, seconds.
 
 """
 #Data Table
